f77bcb90580c5ea9d4b888bedacf0b82591b49e9
GarvSurve <159064494+garvsurve@users.noreply.github.com>
2025-04-10 10:45:10 +0530

    initial commit

---
+++ src/test/java/io/github/garvsurve/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.garvsurve.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
